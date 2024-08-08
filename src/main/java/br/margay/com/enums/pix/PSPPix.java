ab369eb22c8010969399f66b9d329954b6c93871
francisco-vieira <francisco.vieira.dev@gmail.com>
2024-08-08 14:58:21 -0300

    RP-009: retornando ambiente

--- src/main/java/br/margay/com/enums/pix/PSPPix.java
+++ src/main/java/br/margay/com/enums/pix/PSPPix.java
@@ -12,41 +12,42 @@ package br.margay.com.enums.pix;
  */
 public enum PSPPix {
 
-    BRADESCO(1, "Bradesco", "", ""),
-    SICREDI(2, "Sicredi", "", ""),
-    SICOOB(3, "Sicoob", "", ""),
-    SHIPAY(4, "Shipay", "", ""),
-    SANTANDER(5, "Santander", "", ""),
-    PIXDV(6, "PixPDV", "", ""),
-    PAG_SEGURO(7, "PagSeguro", "", ""),
-    ITAU(8, "Itau", "", ""),
-    INTER(9, "Inter", "", ""),
-    EFI(10, "Efi", "https://pix.api.efipay.com.br", "https://pix-h.api.efipay.com.br"), //GerenciaNet
-    BANCO_BRASIL(11, "Banco do Brasil", "", ""),
-    AILOS(12, "Ailos", "", ""),
-    MATERA(13, "Matera", "", ""),
-    CIELO(14, "Cielo", "", ""),
-    MECADO_PAGO(15, "MercadoPago", "", "");
+    BRADESCO(1, "Bradesco", "", "", ""),
+    SICREDI(2, "Sicredi", "", "", ""),
+    SICOOB(3, "Sicoob", "", "", ""),
+    SHIPAY(4, "Shipay", "", "", ""),
+    SANTANDER(5, "Santander", "", "", ""),
+    PIXDV(6, "PixPDV", "", "", ""),
+    PAG_SEGURO(7, "PagSeguro", "", "", ""),
+    ITAU(8, "Itau", "", "", ""),
+    INTER(9, "Inter", "", "", ""),
+    EFI(10, "Efi", "https://pix.api.efipay.com.br", "https://pix-h.api.efipay.com.br", ""), //GerenciaNet
+    BANCO_BRASIL(11, "Banco do Brasil", "", "", ""),
+    AILOS(12, "Ailos", "", "", ""),
+    MATERA(13, "Matera", "", "", ""),
+    CIELO(14, "Cielo", "", "", ""),
+    MECADO_PAGO(15, "MercadoPago", "", "", "");
 
     private final Integer codigo;
     private final String descricao;
     private final String producao;
     private final String homologacao;
+    private final String teste;
 
-    PSPPix(Integer codigo, String descricao, String producao, String homologacao) {
+    PSPPix(Integer codigo, String descricao, String producao, String homologacao, String teste) {
         this.codigo = codigo;
         this.descricao = descricao;
         this.producao = producao;
         this.homologacao = homologacao;
+        this.teste = teste;
     }
 
     public static PSPPix getValueOf(Object object) {
         for (PSPPix e : PSPPix.values()) {
-            if (object instanceof String || object instanceof PSPPix) {
-                if (object.equals(e.toString())) {
+            if ((object instanceof String || object instanceof PSPPix) && object.equals(e.toString())) {
                     return e;
                 }
-            }
+
 
             if (object instanceof Integer) {
                 Integer code = Integer.valueOf(object.toString());
@@ -72,6 +73,26 @@ public enum PSPPix {
         return homologacao;
     }
 
+    public String getTeste() {
+        return teste;
+    }
+
+    /**
+     *
+     * @param code recebe 1 teste, 2 homologacao, 3 producao
+     * @return host do ambiente
+     */
+    public String getAmbiente(int code) {
+        if (code == 1) {
+            return this.teste;
+        } else if (code == 2) {
+            return this.homologacao;
+        } else {
+            return this.producao;
+        }
+    }
+
+    @Override
     public String toString() {
         return descricao;
     }
