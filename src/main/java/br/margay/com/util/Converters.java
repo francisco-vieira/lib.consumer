294eee394b0cdbba9bf5c5cb53327756c93645b5
francisco-vieira <francisco.vieira.dev@gmail.com>
2024-09-06 05:03:00 -0300

    RP-031: Converter String para LocalDateTime

--- src/main/java/br/margay/com/util/Converters.java
+++ src/main/java/br/margay/com/util/Converters.java
@@ -6,30 +6,56 @@
  */
 package br.margay.com.util;
 
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * @author francisco.vieira
  * Criado em 29/05/2024
  */
 public class Converters {
 
-    public static <T> T   objectToClass(Object object, Class<T> clazz) {
+    private Converters() {
+    }
+
+    public static LocalDateTime toLocalDateTime(String dateString) {
+
+        if (StringUtils.isEmpty(dateString)) {
+            return null;
+        }
+        try {
+            ZonedDateTime zonedDateTime = ZonedDateTime.parse(dateString, DateTimeFormatter.ISO_DATE_TIME);
+            return zonedDateTime.toLocalDateTime();
+        } catch (DateTimeParseException e) {
+            Logger.getLogger(Converters.class.getName())
+                    .log(Level.WARNING, e.getMessage(), e);
+            return null;
+        }
+
+    }
+
+    public static <T> T objectToClass(Object object, Class<T> clazz) {
 
         if (!clazz.isInstance(object)) {
-           throw  new UnsupportedOperationException("Object is not an instance of " + clazz.getName());
+            throw new UnsupportedOperationException("Object is not an instance of " + clazz.getName());
         }
         return clazz.cast(object);
     }
 
 
-    public static String charsToString(char[] chars){
-        if (chars == null || chars.length == 0){
+    public static String charsToString(char[] chars) {
+        if (chars == null || chars.length == 0) {
             return "";
         }
         return new String(chars);
     }
 
-    public static char[] stringToChars(String string){
-        if (string == null || string.isEmpty()){
+    public static char[] stringToChars(String string) {
+        if (string == null || string.isEmpty()) {
             return new char[0];
         }
         return string.toCharArray();
