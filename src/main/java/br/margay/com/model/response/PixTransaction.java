a0d764de41e915b4d5e6ed3930c51c56fd4eb9f4
francisco-vieira <francisco.vieira.dev@gmail.com>
2024-09-06 18:59:02 -0300

    RP-037: class TransferPix

---
+++ src/main/java/br/margay/com/model/response/PixTransaction.java
@@ -0,0 +1,31 @@
+/**
+ * Margay Sistemas
+ * https://www.margay.com.br
+ * emails: suporte@margay.com.br, contato@margay.com.br
+ * celular: (93) 99123-4885
+ */
+package br.margay.com.model.response;
+
+import br.margay.com.model.response.pix.Pix;
+
+import java.util.List;
+
+/**
+ * @author francisco.vieira
+ * Criado em 06/09/2024
+ */
+public class PixTransaction {
+
+    private List<Pix> pix;
+
+    // Getters and Setters
+    public List<Pix> getPix() {
+        return pix;
+    }
+
+    public void setPix(List<Pix> pix) {
+        this.pix = pix;
+    }
+
+}
+
